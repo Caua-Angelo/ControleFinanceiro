9a8e4f0c5f5d541a91a90e4be333e6dd0dbc5cff
Cauã Angelo <101962637+Caua-Angelo@users.noreply.github.com>
2026-04-13 22:09:56 -0300

    adicionando testes de integração para AuthController com WebApplicationFactory

---
+++ Controle_Financeiro/Back-End/ControleFinanceiro.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using ControleFinanceiro.Infraestructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+           
+            var descriptors = services.Where(d =>
+                d.ServiceType == typeof(DbContextOptions<ApplicationDBContext>) ||
+                d.ServiceType == typeof(DbContextOptions) ||
+                d.ServiceType.FullName?.Contains("IDbContextOptionsConfiguration") == true
+            ).ToList();
+
+            foreach (var descriptor in descriptors)
+                services.Remove(descriptor);
+
+            services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseInMemoryDatabase("TestDb"));
+        });
+    }
+}
