0d1b66e8ceacbdd5d6eaf8fdecebf2cb21b8f39b
Cauã Angelo <101962637+Caua-Angelo@users.noreply.github.com>
2026-05-01 08:02:22 -0300

    chore: remove rota e import da página de usuário

--- Controle_Financeiro/Front-End/Controle_Financeiro/src/app.tsx
+++ Controle_Financeiro/Front-End/Controle_Financeiro/src/app.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Inicial from "./Pages/index.tsx";
 import Categoria from "./Pages/Categoria.tsx";
-import Usuario from "./Pages/Usuario.tsx";
 import Transacao from "./Pages/Transacao.tsx";
 import Layout from "./Components/layout.tsx";
 import { LoginPage } from "./Pages/LoginPage.tsx";
@@ -24,7 +23,6 @@ function App() {
           <Route index element={<Inicial />} />
           <Route path="categoria" element={<Categoria />} />
           <Route path="transacao" element={<Transacao />} />
-          <Route path="usuario" element={<Usuario />} />
         </Route>
       </Routes>
     </Router>
