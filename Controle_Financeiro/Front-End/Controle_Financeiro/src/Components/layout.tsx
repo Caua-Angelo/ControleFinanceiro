eb15a5e0b4c85c10d72d24e05c60c07ccb5e23d6
Cauã Angelo <101962637+Caua-Angelo@users.noreply.github.com>
2026-05-01 08:03:56 -0300

    fix: corrige comportamento do layout lateral ao trocar de páginas

--- Controle_Financeiro/Front-End/Controle_Financeiro/src/Components/layout.tsx
+++ Controle_Financeiro/Front-End/Controle_Financeiro/src/Components/layout.tsx
@@ -1,6 +1,4 @@
 import { Link, Outlet } from "react-router-dom";
-
-import user from "../assets/user.png";
 import category from "../assets/category.png";
 import Finance from "../assets/finance.png";
 import transaction from "../assets/transaction.png";
@@ -12,9 +10,9 @@ export default function Layout() {
     window.location.href = "/login";
   }
   return (
-    <div className="min-h-screen w-full flex   bg-[#9DB4AB]">
+    <div className="h-screen w-full flex bg-[#9DB4AB]">
       {/* Menu lateral */}
-      <aside className="w-70 bg-[#2F4F4F]/90 backdrop-blur-sm text-white p-5 flex flex-col shadow-[inset_-5px_0_10px_rgba(0,0,0,0.05)]">
+      <aside className="w-72 bg-[#2F4F4F]/90 backdrop-blur-sm text-white p-5 flex flex-col shadow-[inset_-5px_0_10px_rgba(0,0,0,0.05)]">
         <div className="mb-8 flex flex-col items-center">
           <img src={logo} alt="Logo" className="w-12 h-12" />
           <h2 className="text-xl font-semibold mb-4">Controle Financeiro</h2>
@@ -24,11 +22,7 @@ export default function Layout() {
             <img src={Finance} alt="Logo" className="w-6 h-6 mr-2" /> Resumo
           </Link>
         </div>
-        <div className="ml-4">
-          <Link to="/usuario" className={`${menuLink} flex items-center`}>
-            <img src={user} alt="Logo" className="w-6 h-6 mr-2" /> Usuários
-          </Link>
-        </div>
+
         <div className="ml-4">
           <Link to="/categoria" className={`${menuLink} flex items-center`}>
             <img src={category} alt="Logo" className="w-6 h-6 mr-2" /> Categorias
@@ -50,7 +44,7 @@ export default function Layout() {
       {/* Área principal */}
       <div className="flex flex-col flex-1">
         {/* Conteúdo */}
-        <main className="flex-1 overflow-auto p-6  backdrop-blur-sm">
+        <main className="flex-1 overflow-y-auto p-6 backdrop-blur-sm">
           <Outlet />
         </main>
 
