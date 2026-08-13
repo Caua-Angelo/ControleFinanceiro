1964f5511e1188dfdbd909af4a7a2a2d4c57a91e
Cauã Angelo <101962637+Caua-Angelo@users.noreply.github.com>
2026-08-13 09:33:42 -0300

    feat: altera repositório para aceitar id do usuário logado

--- Controle_Financeiro/Back-End/ControleFinanceiro.Domain/Interfaces/ICategoriaRepository.cs
+++ Controle_Financeiro/Back-End/ControleFinanceiro.Domain/Interfaces/ICategoriaRepository.cs
@@ -4,8 +4,8 @@ namespace ControleFinanceiro.Domain.Interfaces
 {
     public interface ICategoriaRepository
     {
-        Task<IEnumerable<Categoria>> ListAsync();
-        Task<Categoria?> GetByIdAsync(int id);
+        Task<IEnumerable<Categoria>> ListAsync(int usuarioId);
+        Task<Categoria?> GetByIdAsync(int id, int usuarioId);
 
         Task AddAsync(Categoria categoria);
         Task DeleteAsync(Categoria categoria);
